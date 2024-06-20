18b24d1074b208e1b057a6628d36e5f7b5169124
thales <thalesquadros2@gmail.com>
2024-06-20 13:54:09 -0300

    style the team component with a list of javascript objects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
+
+  const teams = [
+    {
+      name:"Programação",
+      primaryColor:"#57C278",
+      secundaryColor:"#D9F7E9"
+    },
+    {
+      name:"Frontend",
+      primaryColor:"#82CFFA",
+      secundaryColor:"#E8F8FF"
+    },
+    {
+      name:"Data Science",
+      primaryColor:"#A6D157",
+      secundaryColor:"#F0F8E2"
+    },
+    {
+      name:"DevOps",
+      primaryColor:"#E06B69",
+      secundaryColor:"#FDE7E8"
+    },
+    {
+      name:"Mobile",
+      primaryColor:"#DB6EBF",
+      secundaryColor:"#FAE9F5"
+    },
+    {
+      name:"Inovação e Gestão",
+      primaryColor:"#FFBA05",
+      secundaryColor:"#FFF5D9"
+    },
+    {
+      name:"UX e Design",
+      primaryColor:"#FF8A29",
+      secundaryColor:"#FFEEDF"
+    }
+  ]
 
   const [colaborators, setColaborators] = useState([]);
 
@@ -15,6 +54,14 @@ function App() {
     <div className="App">
       <Banner/>
       <Form onAddedColaborator={colaborator => onNewAddedColaborator(colaborator)}/>
+      {teams.map(team => {
+        return <Team 
+          key={team.name} 
+          name={team.name}
+          primaryColor={team.primaryColor}
+          secundaryColor={team.secundaryColor}
+        />
+      })}
     </div>
   );
 }
---
+++ src/components/Team/index.js
@@ -0,0 +1,19 @@
+import "./Team.css"
+
+const Team = (props) => {
+    return (
+        <section className="team" style={
+            {
+                backgroundColor: props.secundaryColor,
+            }
+        }>
+            <h3 style={
+                {
+                    borderColor: props.primaryColor
+                }
+            }>{props.name}</h3>
+        </section>
+    )
+}
+
+export default Team
